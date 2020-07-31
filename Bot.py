e97f803bca1fe3ddfbc6cb9dac4b9f4cf75ec1f7
Ryan <rk9843@g.rit.edu>
2020-07-30 20:50:16 -0400

    debug button

--- Bot.py
+++ Bot.py
@@ -1,19 +1,60 @@
+import os
+import os.path
 from selenium import webdriver
-from Config import keys
+from webdriver_manager.chrome import ChromeDriverManager
 import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+def setup():
+    configFile = open('Config.txt', 'w+')
+    
+    def getUsername():
+        print('Enter your username:')
+        usr = input()
+        print('Is ' + usr + ' correct? <Y:N>')
+        confirm = input()
+        if (confirm == 'Y'):
+            configFile.write(usr + '\n')
+        else:
+            getUsername()
+  
+    def getPassword():
+        print('Enter your password:')
+        pwd = input()
+        print('Is ' + pwd + ' correct? <Y:N>')
+        confirm = input()
+        if (confirm == 'Y'):
+            configFile.write(pwd + '\n')
+        else:
+            getPassword()
+    
+    getUsername()
+    getPassword()
+    configFile.close()
+
 
 def fillForm():
-    driver.find_element_by_xpath('//*[@id="username"]').send_keys(keys['username'])
-    driver.find_element_by_xpath('//*[@id="password"]').send_keys(keys['password'])
-    driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/form/button').click()
-    driver.switch_to.frame("duo_iframe")
-    driver.find_element_by_xpath('//*[@id="auth_methods"]/fieldset/div[1]/button').click()
-    #driver.switch_to.frame(0)
-    #driver.find_element_by_xpath('//*[@id="portal-navbar"]/ul/li[4]/a').click()
+    configFile = open('Config.txt', 'r')
+    username = configFile.readline().rstrip('\n')
+    password = configFile.readline().rstrip('\n')
+
+    driver.find_element_by_xpath('//*[@id="username"]').send_keys(username)
+    driver.find_element_by_xpath('//*[@id="password"]').send_keys(password)
+    driver.find_element_by_xpath('//*[@id="userInput"]/form/button').click()
+    driver.get('https://dailyhealth.rit.edu/assessment')
+    
+    
     
 
 if __name__ == "__main__":
-    driver = webdriver.Chrome('C:/Users/skim1/chromedriver_win32/chromedriver.exe')
-    driver.get(keys['url'])
+    if not os.path.exists('./Config.txt'):
+        setup()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://dailyhealth.rit.edu/')
     fillForm()
+    time.sleep(10)
     #driver.close()
